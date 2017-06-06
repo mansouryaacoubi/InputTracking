3bdff7549576c5486c5ad32a75385c3b61c56231
Mansour Yaacoubi <mya@qosmotec.com>
2017-06-06 12:47:43 +0200

    First commit

---
+++ InputTracking/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace InputTracking
+{
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer;
+        private AllInputSources lastInput;
+        private KeyboardInput keyboard;
+        private MouseInput mouse;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            keyboard = new KeyboardInput();
+            keyboard.KeyBoardKeyPressed += keyboard_KeyBoardKeyPressed;
+
+            mouse = new MouseInput();
+            mouse.MouseMoved += mouse_MouseMoved;
+
+            lastInput = new AllInputSources();
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        void keyboard_KeyBoardKeyPressed(object sender, EventArgs e)
+        {
+            keyboardTime.Content = FormatDateTime(DateTime.Now);
+        }
+
+        private string FormatDateTime(DateTime dateTime)
+        {
+            return dateTime.ToString("HH:mm:ss fff", CultureInfo.CurrentUICulture);
+        }
+        void mouse_MouseMoved(object sender, EventArgs e)
+        {
+            mouseTime.Content = FormatDateTime(DateTime.Now);
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            lastInputTime.Content = FormatDateTime(lastInput.GetLastInputTime());
+        }
+    }
+}
